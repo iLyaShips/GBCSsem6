f01886ee5c7568109a2803bdf00615b9e061abc8
iLya <andr@google.com>
2022-12-11 20:56:40 +0300

    commit add 001

---
+++ 001/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+void PrintArray(int[] arr)
+{
+    Console.WriteLine("Вывод массива");
+    foreach (int el in arr)
+    {
+        Console.Write(el + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine("Введите элементы массива");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int PosCount (int[]arr)
+{
+    int count = 0;
+    foreach(int el in arr)
+    {
+        if (el > 0 ) count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите число");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArray(m);
+PrintArray(array);
+Console.WriteLine($"В массиве " + PosCount (array) + " положительных чисел.");
